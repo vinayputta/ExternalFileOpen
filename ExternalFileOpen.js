c5565af80e0541ce5ae20d00fe1bbcab753c4d7f
vinayputta <vinay.p@motivitylabs.com>
2014-05-13 10:43:42 +0530

    Update ExternalFileOpen.js

--- ExternalFileOpen.js
+++ ExternalFileOpen.js
@@ -1,23 +1,11 @@
-/*
-THIS SOFTWARE IS PROVIDED BY ANDREW TRICE "AS IS" AND ANY EXPRESS OR
-IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
-EVENT SHALL ANDREW TRICE OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
 
-window.ExternalFileUtil = {
+window.ExternalFileOpen = {
     
     openWith: function ( path, uti, options, success, fail) {
         if(options == undefined)
-            return cordova.exec(success, fail, "ExternalFileUtil", "openWith", [path, uti]);
-        return cordova.exec(success, fail, "ExternalFileUtil", "openWith", [path, uti, options]);
+            return cordova.exec(success, fail, "ExternalFileOpen", "openWith", [path, uti]);
+        return cordova.exec(success, fail, "ExternalFileOpen", "openWith", [path, uti, options]);
     }  
 };
 
-module.exports = ExternalFileUtil;
+module.exports = ExternalFileOpen;
